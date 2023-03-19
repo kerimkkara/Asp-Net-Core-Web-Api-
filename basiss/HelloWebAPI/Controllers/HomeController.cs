645d2f30b9cfda750d950c6374962a38de9b654e
Kerim Kara <kara.kerim@outlook.com>
2023-03-19 09:47:15 +0300

    Create Project

---
+++ basiss/HelloWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebAPI.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public String GetMessage()
+        {
+            return "Hello World";
+        }
+
+    }
+}
--- intro/Program.cs
+++ intro/Program.cs
@@ -1,6 +1,58 @@
 ﻿/*
  * 
+ * API -> application programming ınterface 
+ *
+ * SOAP -> http üzerinden 
+ *
+ * REST
  * 
- *
- *
+ * GraphQL gRPC
+*/
+
+
+/*  
+ 
+APIler presentation katmanı üzerinde yazılır 
+
+
+HTTTP:
+
+GET  = Kaynak İsteme
+Post = Kaynak OLuşturma
+Put = Kaynak Güncelleme
+Patch = Kısmi KaynaK Güncelleme
+Delete = Kaynak Silme
+
+
+Request Yapısı:
+
+Content Type = İçeriğin Formatı
+Content Length = Uzunluk
+
+Status Code:
+
+100-199 -> Bilgi
+200-299 -> Başarı
+300-399 -> Yeniden Yönlendirme
+400-499 -> İstemci Hataları
+500-599 -> Sunucu Hataları
+
+
+
+Rest API Bileşenleri:
+
+Verb - Urı
+     - Headers
+     - Request Body  
+
+URI'ler kaynakların sadece bir parçasıdır
+
+İsimler nouns tercih edilir
+
+Idempotent -> sonucu değiştirmeden defalarca uygulanabilen işlemi ifade 
+eder
+
+Client Url(Curl) -> unix bazlı sistemde mevcut olan bir komuttur
+
+
 */
